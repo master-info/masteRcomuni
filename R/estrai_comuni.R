eb8fb9db6190b3d43c5306f2552632183fd11da3
Luca Valnegri <l.valnegri@datamaps.co.uk>
2022-05-15 20:51:23 +0200

    aggiunta funzione "estrai_comuni"

---
+++ R/estrai_comuni.R
@@ -0,0 +1,61 @@
+#' estrai_comuni
+#' 
+#' Estrae i poligoni di uno o piu' comuni, e se richiesto ne visualizza la mappa 
+#'
+#' @param x codice/i delle localita da estrarre
+#' @param anno anno di riferimento
+#' @param tp tipo delle localita a cui devono associarsi i codici forniti, uno fra: (C)
+#' @param sup indica se devono essere restituiti: 
+#'            (\code{FALSE}) tutti i comuni legati ad \code{x} e \code{tp}
+#'            (\code{TRUE}) esattamente le localita indicate in \code{x} e tipo \code{tp}
+#'
+#' @return un oggetto sf o leaflet
+#'
+#' @author Luca Valnegri, \email{l.valnegri@datamaps.co.uk}
+#'
+#' @import leaflet
+#'
+#' @export
+#'
+estrai_comuni <- function(x, anno = 22, tp = 'CMN', sup = FALSE, mappa = FALSE){
+    tps <- substr(tp, 1, 1)
+    tp <- switch(tps, 'C' = 'CMN', 'P' = 'PRV', 'R' = 'RGN', 'Z' = 'RPT')
+    as <- substring(anno, nchar(anno) - 1)
+    a <- paste0('20', as)
+    if(sup){
+        y <- switch(tps,
+                'C' = get(paste0('C', as)) |> subset(CMN %in% x),
+                'P' = PRV[[a]] |> subset(PRV %in% x) |> merge(provincie[anno == a][, .(PRV, PRVd, PRVs)]),
+                'R' = RGN[[a]] |> subset(RGN %in% x) |> merge(unique(provincie[anno == a][, .(RGN, RGNd)])),
+                'Z' = RPT[[a]] |> subset(RPT %in% x) |> merge(unique(provincie[anno == a][, .(RPT, RPTd)]))
+        ) 
+    } else {
+        yc <- comuni[anno == a][, anno := NULL]
+        y <- get(paste0('C', as)) |> subset(CMN %in% yc[get(tp) %in% x, CMN]) |> merge(yc)
+    }
+    
+    if(mappa){
+        if(sup){
+            leaflet() |> 
+                addTiles() |> 
+                addPolygons(
+                    data = y,
+                    color = 'red',
+                    weight = 5,
+                    fillOpacity = 0,
+                    label = ~paste(get(tp), '-', get(paste0(tp, 'd')))
+                )
+        } else {
+            leaflet() |> 
+                addTiles() |> 
+                addPolygons(
+                    data = y, 
+                    color = 'red',
+                    weight = 5,
+                    fillOpacity = 0,
+                    label = ~paste(CMN, '-', CMNd)
+                )
+        }
+    } else { y }
+        
+}
